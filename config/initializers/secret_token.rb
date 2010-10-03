e7aeb032e70f2e5dac7fa913b6a5e56362a745c1
Cicero Oliveira <cicero@cicerooliveira.com>
2010-10-02 22:42:52 -0400

    first iteration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoStickies::Application.config.secret_token = '4b0042c97b1af8285a24f0afc9432a0a525cb7f57aa120182e1ec7da6f318a940ebb1a4d676edc079081989b4a6c7c1734af56da6fc2e5d2fd92767795d62ee8'

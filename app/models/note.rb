62b574661d55e9d3a8fa6013b7d8841d6b5c9626
Cicero Oliveira <cicero@cicerooliveira.com>
2010-10-02 23:27:03 -0400

    improved layout

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,7 @@ class Note
   include Mongoid::Document
   include Mongoid::Timestamps
   field :text, :type => String
+  field :color, :type => Symbol
+  COLORS = [:yellow, :green, :purple]
+  validates_inclusion_of :color, :in => COLORS
 end
